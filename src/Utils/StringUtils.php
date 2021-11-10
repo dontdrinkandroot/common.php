f0a05deb2fe4918ce85c9398926f36b1308307e5
Philip Washington Sorst <philip@sorst.net>
2021-11-10 19:55:56 +0100

    getFirstChar and getLastChar now returning null instead of false when not found

--- src/Utils/StringUtils.php
+++ src/Utils/StringUtils.php
@@ -1,18 +1,15 @@
 <?php
 
-
 namespace Dontdrinkandroot\Utils;
 
-/**
- * @author Philip Washington Sorst <philip@sorst.net>
- */
 class StringUtils
 {
     /**
      * Checks if a string starts with another string.
      *
      * @param string $haystack The string to search in.
-     * @param string $needle The string to search.
+     * @param string $needle   The string to search.
+     *
      * @return bool
      */
     public static function startsWith(string $haystack, string $needle): bool
@@ -24,7 +21,8 @@ class StringUtils
      * Checks if a string ends with another string.
      *
      * @param string $haystack The string to search in.
-     * @param string $needle The string to search.
+     * @param string $needle   The string to search.
+     *
      * @return bool
      */
     public static function endsWith(string $haystack, string $needle): bool
@@ -36,31 +34,33 @@ class StringUtils
      * Get the first character of a string.
      *
      * @param string $str The string to get the first character of.
-     * @return string|false The first character or false if not found.
+     *
+     * @return string|null The first character or null if not found.
      */
-    public static function getFirstChar($str)
+    public static function getFirstChar(string $str): ?string
     {
         $length = strlen($str);
         if ($length === 0) {
-            return false;
+            return null;
         }
 
-        return substr($str, 0, 1);
+        return $str[0];
     }
 
     /**
      * Get the last character of a string.
      *
      * @param string false The string to get the last character of.
-     * @return string|null The last character or false if not found.
+     *
+     * @return string|null The last character or null if not found.
      */
-    public static function getLastChar($str)
+    public static function getLastChar(string $str): ?string
     {
         $length = strlen($str);
         if ($length === 0) {
-            return false;
+            return null;
         }
 
-        return substr($str, $length - 1, 1);
+        return $str[$length - 1];
     }
 }
