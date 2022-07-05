3b1074d577d0efcc5ce2f50ca9ad8a8cb8e49a19
Philip Washington Sorst <philip@sorst.net>
2022-07-05 11:28:44 +0200

    Extending CollectionUtils

--- src/CollectionUtils.php
+++ src/CollectionUtils.php
@@ -2,6 +2,8 @@
 
 namespace Dontdrinkandroot\Common;
 
+use Exception;
+
 class CollectionUtils
 {
     /**
@@ -39,6 +41,82 @@ class CollectionUtils
             /** @var R $element */
             $element = $element->{$propertyName};
             $results[] = $element;
+        }
+
+        return $results;
+    }
+
+    /**
+     * @template T
+     *
+     * @param iterable<T>           $collection
+     * @param callable(T):array-key $hashFunction
+     *
+     * @return array<array-key,T>
+     */
+    public static function hash(
+        iterable $collection,
+        callable $hashFunction,
+        bool $overrideExisting = false
+    ): array {
+        $results = [];
+        foreach ($collection as $element) {
+            $hashKey = $hashFunction($element);
+            if (!$overrideExisting && array_key_exists($hashKey, $results)) {
+                throw new Exception('HashKey "' . $hashKey . '" already exists.');
+            }
+            $results[$hashKey] = $element;
+        }
+
+        return $results;
+    }
+
+    /**
+     * @template T
+     *
+     * @param iterable<T> $collection
+     * @param string      $propertyName
+     *
+     * @return array<array-key,T>
+     */
+    public static function hashByProperty(
+        iterable $collection,
+        string $propertyName,
+        bool $overrideExisting = false
+    ): array {
+        $results = [];
+        foreach ($collection as $element) {
+            $hashKey = $element->{$propertyName};
+            if (!is_string($hashKey) && !is_int($hashKey)) {
+                throw new Exception(((string)$hashKey) . ' is not a valid array-key');
+            }
+            if (!$overrideExisting && array_key_exists($hashKey, $results)) {
+                throw new Exception('HashKey "' . $hashKey . '" already exists.');
+            }
+            $results[$hashKey] = $element;
+        }
+
+        return $results;
+    }
+
+    /**
+     * @param array<array-key, array> $arr
+     * @param array-key               $key
+     *
+     * @return array
+     */
+    public static function hashByKey(array $arr, int|string $key, bool $overrideExisting = false): array
+    {
+        $results = [];
+        foreach ($arr as $element) {
+            $hashKey = $element[$key];
+            if (!is_string($hashKey) && !is_int($hashKey)) {
+                throw new Exception(((string)$hashKey) . ' is not a valid array-key');
+            }
+            if (!$overrideExisting && array_key_exists($hashKey, $results)) {
+                throw new Exception('HashKey "' . $hashKey . '" already exists.');
+            }
+            $results[$hashKey] = $element;
         }
 
         return $results;
--- test/CollectionUtilsTest.php
+++ test/CollectionUtilsTest.php
@@ -2,6 +2,7 @@
 
 namespace Dontdrinkandroot\Common;
 
+use Exception;
 use PHPUnit\Framework\TestCase;
 
 class CollectionUtilsTest extends TestCase
@@ -35,5 +36,71 @@ class CollectionUtilsTest extends TestCase
 
         $result = CollectionUtils::collectProperty($collection, 'stringProperty');
         $this->assertEquals(['a', 'c', 'b'], $result);
+    }
+
+    public function testHash(): void
+    {
+        $collection = [];
+        $element1 = new SimplePopo('a', 1);
+        $collection[] = $element1;
+        $element2 = new SimplePopo('c', 3);
+        $collection[] = $element2;
+        $element3 = new SimplePopo('b', 2);
+        $collection[] = $element3;
+
+        $result = CollectionUtils::hash($collection, fn(SimplePopo $simplePopo) => $simplePopo->intProperty);
+        $this->assertEquals($result[1], $element1);
+        $this->assertEquals($result[2], $element3);
+        $this->assertEquals($result[3], $element2);
+    }
+
+    public function testHashByProperty(): void
+    {
+        $collection = [];
+        $element1 = new SimplePopo('a', 1);
+        $collection[] = $element1;
+        $element2 = new SimplePopo('c', 3);
+        $collection[] = $element2;
+        $element3 = new SimplePopo('b', 2);
+        $collection[] = $element3;
+
+        $result = CollectionUtils::hashByProperty($collection, 'intProperty');
+        $this->assertEquals($result[1], $element1);
+        $this->assertEquals($result[2], $element3);
+        $this->assertEquals($result[3], $element2);
+    }
+
+    public function testHashByKey(): void
+    {
+        $results = CollectionUtils::hashByKey([['key' => 'a'], ['key' => 'b'],], 'key');
+        $this->assertEquals(['a' => ['key' => 'a'], 'b' => ['key' => 'b']], $results);
+    }
+
+    public function testHashByKeyWithDuplicate(): void
+    {
+        $this->expectException(Exception::class);
+        CollectionUtils::hashByKey([['key' => 'a'], ['key' => 'b'], ['key' => 'a']], 'key');
+    }
+
+    public function testHashByKeyWithDuplicateAllowed(): void
+    {
+        $results = CollectionUtils::hashByKey([
+            ['key' => 'a', 'value' => 1],
+            ['key' => 'b', 'value' => 2],
+            ['key' => 'a', 'value' => 3],
+        ], 'key', true);
+        $this->assertEquals(
+            [
+                'a' => ['key' => 'a', 'value' => 3],
+                'b' => ['key' => 'b', 'value' => 2],
+            ],
+            $results
+        );
+    }
+
+    public function testHashByKeyWithInvalidValue(): void
+    {
+        $this->expectException(Exception::class);
+        CollectionUtils::hashByKey([['key' => 3.1], ['key' => 'b'], ['key' => 'a']], 'key');
     }
 }
